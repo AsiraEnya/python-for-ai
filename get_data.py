97bc7c1b3075f6e2417d6b4a2a64f8d1265722e0
Asira Enya <asiraenya@gmail.com>
2026-06-29 10:29:52 +0100

    updates

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Calculate dates
+# Calculate dates for the past week
 today = datetime.now()
 week_ago = today - timedelta(days=7)
 
